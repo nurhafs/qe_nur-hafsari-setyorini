3fcc6172cc2ef744fe7c6003a8b6aee0cb0d4ecb
Nur Hafsari Setyorini <nur.hafsari@ui.ac.id>
2022-09-15 17:24:15 +0700

    section 12: edited praktikum

--- 12_Object Oriented Programming/Praktikum/OngkosKirim.java
+++ 12_Object Oriented Programming/Praktikum/OngkosKirim.java
@@ -2,25 +2,29 @@ import java.lang.Math;
 import java.util.Scanner;
 
 public class OngkosKirim {
-    private static void ongkir(double p, double l, double t, double berat) {
-        double volume = p*l*t;
-        double harga = ((volume / 50) + (berat / 1)) * 2500;
+    private static void ongkir(int p, int l, int t, int berat) {
+        int volume = p*l*t;
+        int harga = ((volume / 50) + 1) * 5000;
         if (harga < 5000) {
             harga = 5000;
         }
+        // if (berat > 1) {
+        //     harga += (berat-1) * 5000; //asumsi untuk setiap kenaikan 1 kg paket akan dikenakan biaya 5000
+        // }
         System.out.println("Harga: " + (int)harga);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Panjang = ");
-        double p = sc.nextDouble();
+        int p = sc.nextInt();
         System.out.print("Lebar = ");
-        double l = sc.nextDouble();
+        int l = sc.nextInt();
         System.out.print("Tinggi = ");
-        double t = sc.nextDouble();
+        int t = sc.nextInt();
         System.out.print("Berat = " );
-        double berat = sc.nextDouble();
+        int berat = sc.nextInt();
         ongkir(p,l,t,berat);
+        sc.close();
     }
 }
